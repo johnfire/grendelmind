05051b83352c48a710e92a7f6e9dd964685e3bb1
christopherrehm <christopherrehm@squeaker>
2017-07-01 18:11:26 +0200

    first for this dir Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ src/analyzers/firstAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analyzers;
+
+import basicstuff.*;
+
+/**
+ *
+ * @author christopherrehm
+ */
+public class firstAnalyzer extends basicObject implements Runnable{
+    
+}
