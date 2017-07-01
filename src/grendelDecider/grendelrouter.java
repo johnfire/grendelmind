b4b45bd502db55a56628387d9cbacf3bdd8c73cf
christopherrehm <christopherrehm@squeaker>
2017-07-01 18:09:21 +0200

    first for this dir Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ src/grendelDecider/grendelrouter.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grendelDecider;
+
+import basicstuff.*;
+
+/**
+ *
+ * @author christopherrehm
+ */
+public class grendelrouter extends basicObject implements Runnable {
+    
+}
