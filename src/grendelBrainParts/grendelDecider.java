54c0103d9b50db6a29d9a476321349ae9e8a108b
christopherrehm <christopherrehm@squeaker>
2017-07-01 18:18:54 +0200

    first for this dir Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelBrainParts/grendelDecider.java
+++ src/grendelBrainParts/grendelDecider.java
@@ -6,11 +6,37 @@
 package grendelBrainParts;
 
 import basicstuff.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author christopherrehm
  */
 public class grendelDecider extends basicObject implements Runnable {
+    int pid;
+    int runVar = 1;
     
+    @Override
+    public void run() {   
+    System.out.println("we are in the grendelDecider routine");
+ 
+        try {
+             pid = Integer.parseInt(new File("/proc/self").getCanonicalFile().getName());
+        } catch (IOException ex) {
+            Logger.getLogger(grendelDecider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("we are in the grendelDecider routine first time"+ "my process ID is "+ pid);
+        while (runVar== 1){
+            try {
+                Thread.sleep(5000);
+                System.out.println("we are in the grendelDecider routine, process number "+pid);  
+            } catch (InterruptedException ex) {
+                Logger.getLogger(grendelDecider.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }  
+    }
 }
--- src/grendelBrainParts/grendelrouter.java
+++ src/grendelBrainParts/grendelrouter.java
@@ -6,11 +6,36 @@
 package grendelBrainParts;
 
 import basicstuff.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author christopherrehm
  */
 public class grendelrouter extends basicObject implements Runnable {
+    int pid;
+    int runVar = 1;
+    @Override
+    public void run() {
+        System.out.println("we are in the grendelRouter routine");
+        try {
+             pid = Integer.parseInt(new File("/proc/self").getCanonicalFile().getName());
+        } catch (IOException ex) {
+            Logger.getLogger(grendelrouter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("we are in the grendelRouter routine first time"+ "my process ID is "+ pid);
+        while (runVar== 1){
+            try {
+                Thread.sleep(5000);
+                System.out.println("we are in the grendelRouter routine, process number "+pid);  
+            } catch (InterruptedException ex) {
+                Logger.getLogger(grendelrouter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }    
     
 }
