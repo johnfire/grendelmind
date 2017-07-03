dde9f8fd0025b1d88a7cf4a991cf40ff99df4976
christopherrehm <christopherrehm@squeaker>
2017-07-03 03:39:52 +0200

    repairing dir Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelBrainParts/grendelrouter.java
+++ src/grendelBrainParts/grendelrouter.java
@@ -8,6 +8,7 @@ package grendelBrainParts;
 import basicstuff.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,27 +16,46 @@ import java.util.logging.Logger;
  *
  * @author christopherrehm
  */
-public class grendelrouter extends basicObject implements Runnable {
+public class grendelrouter extends basicObject {
+    
     int pid;
-    int runVar = 1;
+    int port = 5000;
+
+    /**
+     *Set up a blank linked list for use by processor
+     */
+    public LinkedList<message> unProcessedMessages;
+
+    public grendelrouter() {
+        this.unProcessedMessages = new LinkedList();
+    }
+    
     @Override
     public void run() {
+        //create a server mem space
+        //GreetingServer myServer = null;
+        
+        ObjectStatus mystats = new basicstuff.ObjectStatus();
+        mystats.setMyName("grendelRouter");
+        Thread routerThread = new Thread(mystats);
+        routerThread.start();
+        
         System.out.println("we are in the grendelRouter routine");
+        // get this process number
         try {
              pid = Integer.parseInt(new File("/proc/self").getCanonicalFile().getName());
         } catch (IOException ex) {
             Logger.getLogger(grendelrouter.class.getName()).log(Level.SEVERE, null, ex);
         }
+        //create server and run
+        GreetingServer myServer = new GreetingServer();
         
-        System.out.println("we are in the grendelRouter routine first time"+ "my process ID is "+ pid);
-        while (runVar== 1){
-            try {
-                Thread.sleep(5000);
-                System.out.println("we are in the grendelRouter routine, process number "+pid);  
-            } catch (InterruptedException ex) {
-                Logger.getLogger(grendelrouter.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }    
-    
+        Thread theServerThread = new Thread(myServer);
+        theServerThread.start();
+        System.out.println("-----System Message- started server thread-----");
+    }
+
+    public void addMessage(message aMessage){
+        this.unProcessedMessages.addLast(aMessage);    
+    }
 }
