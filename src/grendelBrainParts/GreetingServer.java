d2e820dda12e2219b4b8bd10b6a7ff0765326c50
christopherrehm <christopherrehm@squeaker>
2017-07-04 18:39:29 +0200

    clean up
    Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelBrainParts/GreetingServer.java
+++ src/grendelBrainParts/GreetingServer.java
@@ -31,25 +31,19 @@ import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/** greeting server contains all the server functionality
+ * 
+ * @author christopherrehm
+ */
+
 public class GreetingServer extends BasicObject {
 
-    allLinkedLists theRouter = new allLinkedLists();
-
-    GreetingServer(allLinkedLists aThis) {
-       this.theRouter = aThis;
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-//    GreetingServer(LinkedList<Message> unProcessedMessages) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-    
-
+    allLinkedLists thelinkedListObject = new allLinkedLists();
     private ServerSocket serverSocket;
 
-//    GreetingServer(allLinkedLists myLinkedLists) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
+    GreetingServer(allLinkedLists aThis) {
+       this.thelinkedListObject = aThis;
+    }
    
     /**
      *
@@ -61,7 +55,7 @@ public class GreetingServer extends BasicObject {
             serverSocket = new ServerSocket(5000);
             this.systemMessageStartUp("-----Greeting Server----- started the ServerSocket-----");
             while (true) {
-                new EchoClientHandler(serverSocket.accept(), this.theRouter).start();
+                new EchoClientHandler(serverSocket.accept(), this.thelinkedListObject).start();
                 this.systemMessageStartUp("-----Greeting Server----- started new echo server handler");
             }
         } catch (IOException e) {
@@ -91,7 +85,7 @@ public class GreetingServer extends BasicObject {
         @Override
         public void run() {
             LinkedList<Message> myMessageHolder = new LinkedList();
-            System.out.println("-----*** in echoClientHandlerServer ***----- System Message Entering client handler");
+            System.out.println("-----*** in echoIndyServer ***----- System Message Entering client handler");
             
             try {
                 
@@ -100,7 +94,7 @@ public class GreetingServer extends BasicObject {
                 
                 try {
                     firstMessage = (Message)inFromClient.readObject();
-                    System.out.println("-----*** in echoserver ***----- address of first message is"+ firstMessage);
+                    System.out.println("-----*** in echoIndyServer ***----- address of first message is"+ firstMessage);
                     this.theLinkedListObject.unProcessedMessages.add(firstMessage);
                     // test to see if lock connection neeeds to be set
                     if(LockConnection == false) {// lock this handler to the correct sender
@@ -109,7 +103,7 @@ public class GreetingServer extends BasicObject {
                     }
                 } catch (ClassNotFoundException ex) {
                     Logger.getLogger(GreetingServer.class.getName()).log(Level.SEVERE, null, ex);
-                    System.out.println("-----*** echo server ***----- ERROR DID NOT GET FIRST MESSAGE!!!!!");
+                    System.out.println("-----*** in echoIndyServer ***----- ERROR DID NOT GET FIRST MESSAGE!!!!!");
                 }
                
                 // my name is set, now enter loop and send and receive messages
